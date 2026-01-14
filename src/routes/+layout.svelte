46a0e4db0b77c58e03a22e1ffa65950e77f7513b
jony <jonylg@live.com>
2026-01-14 10:06:14 +0100

    Commit initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.ico';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
+
+
+<style>
+
+	:global(*) {
+	  margin: 0;
+	  padding: 0;
+	  outline: 0;
+	  box-sizing: border-box;
+	}
+	
+	:global(html){
+		font-size: 16px;
+		scroll-behavior: smooth;
+	}
+
+	:global(body){
+	 background:url(/assets/img/campo_de_futbol.avif);
+	 background-repeat: no-repeat;
+	 background-position: center;
+	 background-size: cover;
+	 color: white;
+	 height: 109vh;
+	 -webkit-font-smoothing: antialiased;
+	}
+	
+	:root{
+		--color-primary:white;
+		--color-secundary:red;
+	}
+
+	:global(body, input, button) {
+
+	 font-family:'Times New Roman', Times, serif ;
+	}
+	
+	:global(button) {
+	 cursor: pointer;
+	}
+
+	@font-face{
+		font: family EASPORTS 15ttf;
+		src: url(./Fonts/ea_font/EASPORTS15.ttf)}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,132 @@
+<script>
+
+const equipos = 'https://www.thesportsdb.com/api/v1/json/123/search_all_teams.php?l=Spanish_tercera_federacion_group_1';
+
+
+const fetchEquipos = async () =>{
+
+    const response = await fetch(equipos);
+    const data = await response.json();
+
+    return await data.teams;
+ };
+
+
+ let promise = fetchEquipos();
+
+
+
+</script>
+
+<section class="teams">
+  <div class="teams__tittle">
+    <h2 class="teams__h2">ESCOLLE O TEU CLUBE
+
+    </h2>
+  </div>
+
+  <div class="teams__cards">
+
+{#await promise}
+
+    <p>Cargando equipos ...</p>
+{:then equiposLiga} 
+    
+{#each equiposLiga as team }
+    <!-- inicia card -->
+     <a class="teams__a" href="/{team.idTeam}">
+  <div class="teams__container">
+    <div class="teams__card">
+      <div class="teams__badge">
+        <img class="teams__img" src={team.strBadge} alt={team.strTeam} />
+      </div>
+    </div>
+      <div class="teams__name">
+        <p>{team.strTeam}</p>
+      </div>
+<!-- finaliza cada card -->
+  </div>
+  </a>
+{/each}
+
+  
+  {/await}
+    </div>
+    <button class="teams__button">
+        SELECCIONA UN CLUBE
+    </button>
+</section>
+
+<style>
+.teams{
+padding: 1.1rem;
+}
+
+.teams__tittle{
+width: 100%;
+}
+
+.teams__h2{
+    text-align: center;
+    font-size: 1.4rem;
+}
+
+.teams__cards{
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+    justify-content: center;
+}
+
+.teams__container{
+    display: flex;
+    gap: 1px;
+    flex-direction: column;
+    align-items: center;
+    width: 94px;
+}
+
+
+.teams__card{
+
+    width: 99px;
+    height: 99px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.teams__badge{
+    border-radius: 10px;
+    height: 30px;
+    width: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+
+.teams__img{
+    max-width: 75px;
+}
+
+.teams__cards{
+padding: 1rem 0px;
+}
+
+.teams__button{
+    width: 100%;
+    font-size: 1rem;
+    padding: 1rem;
+    border-radius: 8px;
+
+
+}
+
+.teams__name{
+    text-align: center;
+    height: 50px;
+    color: rgb(187, 187, 187);
+}
+
+</style>
